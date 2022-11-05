2986cc699a3f83fa0ddae2972fcc60a7ff525681
abdulwahab <mrwahab308@gmail.com>
2022-11-05 01:01:06 -0700

    1:01

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': {'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px'},
+    },
+    
+    fontFamily: {
+      'kanit':['Kanit']
+    }
+   
+  },
+  plugins: [],
+}

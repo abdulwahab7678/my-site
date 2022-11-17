035d4b4b13649b4204c3ff28d6ff51e750952dda
abdulwahab <mrwahab308@gmail.com>
2022-11-17 19:37:33 +0500

    7:37

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
-console.clear();
+
 const makeNavLinksSmooth = () => {
   const navLinks = document.querySelectorAll(".nav-link");
   for (let n in navLinks) {
-    //console.log(navLinks[n].hash);
     if (navLinks.hasOwnProperty(n)) {
       navLinks[n].addEventListener("click", (e) => {
         e.preventDefault();
@@ -27,10 +26,55 @@ const spyScrolling = () => {
         document.querySelector(".active").classList.remove("active");
         document
           .querySelector(`a[href*=${id}]`)
-          .parentNode.classList.add("active");
+          .classList.add("active");
       }
   };
 };
 
 makeNavLinksSmooth();
 spyScrolling();
+
+
+
+
+
+
+
+const setDarkMode = (active = false) => {
+  const wrapper = document.querySelector(":root");
+  if (active) {
+    wrapper.setAttribute("data-theme", "dark");
+    // localStorage.setItem("theme", "dark");
+  } else {
+    wrapper.setAttribute("data-theme", "light");
+    // localStorage.setItem("theme", "light");
+  }
+};
+
+const toggleDarkMode = () => {
+  const theme = document.querySelector(":root").getAttribute("data-theme");
+  // If the current theme is "light", we want to activate dark
+  setDarkMode(theme === "light");
+};
+
+const initDarkMode = () => {
+  const query = window.matchMedia("(prefers-color-scheme: dark)");
+  // const themePreference = localStorage.getItem("theme");
+
+  let active = query.matches;
+  // if (themePreference === "dark") {
+  //   active = true;
+  // }
+  // if (themePreference === "light") {
+  //   active = false;
+  // }
+
+  setDarkMode(active);
+
+  query.addListener(e => setDarkMode(e.matches));
+
+  const toggleButton = document.querySelector(".js__dark-mode-toggle");
+  toggleButton.addEventListener("click", toggleDarkMode);
+};
+
+initDarkMode();
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import './style.css'
+import './style2.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import './font.css'
 import 'flowbite/';

5c8f7ac3a89df9dc411e4400f4b46a9b618a13fa
abdulwahab <mrwahab308@gmail.com>
2022-11-19 16:13:08 +0500

    4:13

--- code.js
+++ code.js
@@ -121,3 +121,94 @@ form.addEventListener("submit", function (e) {
       }, 5000);
     });
 });
+
+
+
+
+
+// // We select the element we want to target
+// var target = document.querySelector("footer");
+
+// var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+// var rootElement = document.documentElement;
+
+// // Next we want to create a function that will be called when that element is intersected
+// function callback(entries, observer) {
+//   // The callback will return an array of entries, even if you are only observing a single item
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       // Show button
+//       scrollToTopBtn.classList.add("showBtn");
+//     } else {
+//       // Hide button
+//       scrollToTopBtn.classList.remove("showBtn");
+//     }
+//   });
+// }
+
+// function scrollToTop() {
+//   rootElement.scrollTo({
+//     top: 0,
+//     behavior: "smooth"
+//   });
+// }
+// scrollToTopBtn.addEventListener("click", scrollToTop);
+
+// // Next we instantiate the observer with the function we created above. This takes an optional configuration
+// // object that we will use in the other examples.
+// let observer = new IntersectionObserver(callback);
+// // Finally start observing the target element
+// observer.observe(target);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+document.addEventListener('DOMContentLoaded', function() {
+  const goTopButton = document.querySelector('[data-action="gotop"]');
+  const windowViewPortHeight = window.innerHeight; // browser viewport height
+  let isRequestingAnimationFrame = false;
+
+  if (!goTopButton) {
+    return;
+  }
+
+  goTopButton.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('scroll', function() {
+    if (!isRequestingAnimationFrame) {
+      requestAnimationFrame(filterGoTopButtonVisibility);
+      isRequestingAnimationFrame = true;
+    }
+  });
+
+  function filterGoTopButtonVisibility(timestamp) {
+    let windowPageYOffset = window.pageYOffset || document.documentElement.scrollTop;
+    if (windowPageYOffset > windowViewPortHeight) {
+      goTopButton.classList.add('showBtn');
+      isRequestingAnimationFrame = false;
+    } else {
+      goTopButton.classList.remove('showBtn');
+      requestAnimationFrame(filterGoTopButtonVisibility);
+    }
+  }
+})
+

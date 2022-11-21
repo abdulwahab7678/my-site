6d0bd376e1a2a67d4512d63ccf3ad66da222b471
abdulwahab <mrwahab308@gmail.com>
2022-11-21 21:10:34 +0500

    9:10

--- code.js
+++ code.js
@@ -38,42 +38,7 @@ spyScrolling();
 
 
 
-
-
-// darkAndLightMode
-
-// const setDarkMode = (active = false) => {
-//   const wrapper = document.querySelector(":root");
-//   if (active) {
-//     wrapper.setAttribute("data-theme", "dark");
-//     // localStorage.setItem("theme", "dark");
-//   } else {
-//     wrapper.setAttribute("data-theme", "light");
-//     // localStorage.setItem("theme", "light");
-//   }
-// };
-
-// const toggleDarkMode = () => {
-//   const theme = document.querySelector(":root").getAttribute("data-theme");
-//   // If the current theme is "light", we want to activate dark
-//   setDarkMode(theme === "light");
-// };
-
-// const initDarkMode = () => {
-//   const query = window.matchMedia("(prefers-color-scheme: dark)");
-//   let active = query.matches;
-//   setDarkMode(active);
-
-//   query.addListener(e => setDarkMode(e.matches));
-
-//   const toggleButton = document.querySelector(".js__dark-mode-toggle");
-//   toggleButton.addEventListener("click", toggleDarkMode);
-// };
-
-// initDarkMode();
-
-
-
+// darkMode
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
@@ -132,39 +97,7 @@ form.addEventListener("submit", function (e) {
 
 
 
-// // We select the element we want to target
-// var target = document.querySelector("footer");
 
-// var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
-// var rootElement = document.documentElement;
-
-// // Next we want to create a function that will be called when that element is intersected
-// function callback(entries, observer) {
-//   // The callback will return an array of entries, even if you are only observing a single item
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       // Show button
-//       scrollToTopBtn.classList.add("showBtn");
-//     } else {
-//       // Hide button
-//       scrollToTopBtn.classList.remove("showBtn");
-//     }
-//   });
-// }
-
-// function scrollToTop() {
-//   rootElement.scrollTo({
-//     top: 0,
-//     behavior: "smooth"
-//   });
-// }
-// scrollToTopBtn.addEventListener("click", scrollToTop);
-
-// // Next we instantiate the observer with the function we created above. This takes an optional configuration
-// // object that we will use in the other examples.
-// let observer = new IntersectionObserver(callback);
-// // Finally start observing the target element
-// observer.observe(target);
 
 
 
@@ -220,7 +153,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-const menuBtn = document.querySelector(".menuBtn")
+const menuBtn = document.querySelector("#menuToggle")
 const navbar = document.querySelector(".nav-ul-p")
 
 menuBtn.addEventListener("click", ()=>{

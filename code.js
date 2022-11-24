9fc8df542009f0871954f9a01b3605dfb4e9869a
abdulwahab <mrwahab308@gmail.com>
2022-11-24 16:15:34 +0500

    4:14

--- code.js
+++ code.js
@@ -37,83 +37,6 @@ spyScrolling();
 
 
 
-
-// darkMode
-const checkbox = document.getElementById("checkbox")
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
-})
-
-
-// form
-
-// const form = document.getElementById("form");
-// const result = document.getElementById("result");
-
-// form.addEventListener("submit", function (e) {
-//   const formData = new FormData(form);
-//   e.preventDefault();
-//   var object = {};
-//   formData.forEach((value, key) => {
-//     object[key] = value;
-//   });
-//   var json = JSON.stringify(object);
-//   result.innerHTML = "Please wait...";
-
-//   fetch("https://api.web3forms.com/submit", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json"
-//     },
-//     body: json
-//   })
-//     .then(async (response) => {
-//       let json = await response.json();
-//       if (response.status == 200) {
-//         result.innerHTML = json.message;
-//         result.classList.remove("text-gray-500");
-//         result.classList.add("text-green-500");
-//       } else {
-//         console.log(response);
-//         result.innerHTML = json.message;
-//         result.classList.remove("text-gray-500");
-//         result.classList.add("text-red-500");
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       result.innerHTML = "Something went wrong!";
-//     })
-//     .then(function () {
-//       form.reset();
-//       setTimeout(() => {
-//         result.style.display = "none";
-//       }, 5000);
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // goTopBtn
 
 let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
@@ -154,6 +77,12 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 
+
+// darkMode
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
 
 
 // menuNavbar
